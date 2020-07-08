ae5d2b1ceefb7861248b9984fd4681f92f14d8f3
Hoanguitk12 <44947905+Hoanguitk12@users.noreply.github.com>
2020-07-08 13:10:37 +0700

    cameraview

---
+++ panzitzoom/panzitzoom/CameraViewer.xaml.cs
@@ -0,0 +1,332 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Forms;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using Ozeki.Camera;
+using Ozeki.Media;
+using Ozeki.VoIP;
+namespace panzitzoom
+{
+    /// <summary>
+    /// Interaction logic for CameraViewer.xaml
+    /// </summary>
+    public partial class CameraViewer : Window
+    {
+        // khoi tao các camera
+        private IIPCamera _camera1;
+        private DrawingImageProvider _drawingImageProvider1;
+        private MediaConnector _connector1;
+        private MPEG4Recorder _mpeg4Recorder1;
+        private FolderBrowserDialog _folderBrowserDialog1;
+
+        private IIPCamera _camera2;
+        private DrawingImageProvider _drawingImageProvider2;
+        private MediaConnector _connector2;
+        private MPEG4Recorder _mpeg4Recorder2;
+        private FolderBrowserDialog _folderBrowserDialog2;
+
+        private IIPCamera _camera3;
+        private DrawingImageProvider _drawingImageProvider3;
+        private MediaConnector _connector3;
+        private MPEG4Recorder _mpeg4Recorder3;
+        private FolderBrowserDialog _folderBrowserDialog3;
+
+        private IIPCamera _camera4;
+        private DrawingImageProvider _drawingImageProvider4;
+        private MediaConnector _connector4;
+        private MPEG4Recorder _mpeg4Recorder4;
+        private FolderBrowserDialog _folderBrowserDialog4;
+        public CameraViewer()
+        {
+            InitializeComponent();
+            //cam1
+            _drawingImageProvider1 = new DrawingImageProvider();
+            _connector1 = new MediaConnector();
+            viewVid1.SetImageProvider(_drawingImageProvider1);
+            _folderBrowserDialog1 = new FolderBrowserDialog();
+            //cam2
+            _drawingImageProvider2 = new DrawingImageProvider();
+            _connector2 = new MediaConnector();
+            viewVid2.SetImageProvider(_drawingImageProvider2);
+            _folderBrowserDialog2 = new FolderBrowserDialog();
+            //cam3
+            _drawingImageProvider3 = new DrawingImageProvider();
+            _connector3 = new MediaConnector();
+            viewVid3.SetImageProvider(_drawingImageProvider3);
+            _folderBrowserDialog3 = new FolderBrowserDialog();
+            //cam4
+            _drawingImageProvider4 = new DrawingImageProvider();
+            _connector4 = new MediaConnector();
+            viewVid4.SetImageProvider(_drawingImageProvider4);
+            _folderBrowserDialog4 = new FolderBrowserDialog();
+        }
+        // luong xu ly cam1
+        private void Record1_Click(object sender, RoutedEventArgs e)
+        {
+            var path = txtPath1.Text;
+            if (!String.IsNullOrEmpty(path))
+                StartVideoCapture1(path);
+        }
+
+        private void Stop1_Click(object sender, RoutedEventArgs e)
+        {
+            StopVideoCapture1();
+        }
+
+        private void ConnectCam1_Click(object sender, RoutedEventArgs e)
+        {
+            _camera1 = IPCameraFactory.GetCamera(IpCam1.Text, UseCam1.Text, PassCam1.Text);
+
+            _connector1.Connect(_camera1.VideoChannel, _drawingImageProvider1);
+            _camera1.Start();
+            viewVid1.Start();
+        }
+
+        private void FilePath1_Click(object sender, RoutedEventArgs e)
+        {
+            var result = _folderBrowserDialog1.ShowDialog();
+            if (result == System.Windows.Forms.DialogResult.OK)
+                txtPath1.Text = _folderBrowserDialog1.SelectedPath;
+        }
+
+        private void StartVideoCapture1(string path)
+        {
+            var date = DateTime.Now.Year + "y-" + DateTime.Now.Month + "m-" + DateTime.Now.Day + "d-" +
+                       DateTime.Now.Hour + "h-" + DateTime.Now.Minute + "m-" + DateTime.Now.Second + "s";
+            string currentpath;
+            if (String.IsNullOrEmpty(path))
+                currentpath = date + ".mp4";
+            else
+                currentpath = path + "\\" + date + ".mp4";
+
+            _mpeg4Recorder1 = new MPEG4Recorder(currentpath);
+            _mpeg4Recorder1.MultiplexFinished += Mpeg4Recorder_MultiplexFinished1;
+            _connector1.Connect(_camera1.AudioChannel, _mpeg4Recorder1.AudioRecorder);
+            _connector1.Connect(_camera1.VideoChannel, _mpeg4Recorder1.VideoRecorder);
+        }
+
+        private void Mpeg4Recorder_MultiplexFinished1(object sender, VoIPEventArgs<bool> e)
+        {
+            _connector1.Disconnect(_camera1.AudioChannel, _mpeg4Recorder1.AudioRecorder);
+            _connector1.Disconnect(_camera1.VideoChannel, _mpeg4Recorder1.VideoRecorder);
+            _mpeg4Recorder1.Dispose();
+        }
+
+        
+
+        private void StopVideoCapture1()
+        {
+            if (_mpeg4Recorder1 != null)
+            {
+                _mpeg4Recorder1.Multiplex();
+                _connector1.Disconnect(_camera1.AudioChannel, _mpeg4Recorder1.AudioRecorder);
+                _connector1.Disconnect(_camera1.VideoChannel, _mpeg4Recorder1.VideoRecorder);
+            }
+        }
+
+        // luong xu ly cam 2
+        private void Record2_Click(object sender, RoutedEventArgs e)
+        {
+            var path = txtPath2.Text;
+            if (!String.IsNullOrEmpty(path))
+                StartVideoCapture2(path);
+        }
+
+        private void Stop2_Click(object sender, RoutedEventArgs e)
+        {
+            StopVideoCapture2();
+        }
+
+        private void ConnectCam2_Click(object sender, RoutedEventArgs e)
+        {
+            _camera2 = IPCameraFactory.GetCamera(IpCam2.Text, UseCam2.Text, PassCam2.Text);
+
+            _connector2.Connect(_camera2.VideoChannel, _drawingImageProvider2);
+            _camera2.Start();
+            viewVid2.Start();
+        }
+
+        private void FilePath2_Click(object sender, RoutedEventArgs e)
+        {
+            var result = _folderBrowserDialog2.ShowDialog();
+            if (result == System.Windows.Forms.DialogResult.OK)
+                txtPath2.Text = _folderBrowserDialog2.SelectedPath;
+        }
+        private void StartVideoCapture2(string path)
+        {
+            var date = DateTime.Now.Year + "y-" + DateTime.Now.Month + "m-" + DateTime.Now.Day + "d-" +
+                       DateTime.Now.Hour + "h-" + DateTime.Now.Minute + "m-" + DateTime.Now.Second + "s";
+            string currentpath;
+            if (String.IsNullOrEmpty(path))
+                currentpath = date + ".mp4";
+            else
+                currentpath = path + "\\" + date + ".mp4";
+
+            _mpeg4Recorder2 = new MPEG4Recorder(currentpath);
+            _mpeg4Recorder2.MultiplexFinished += Mpeg4Recorder_MultiplexFinished2;
+            _connector2.Connect(_camera2.AudioChannel, _mpeg4Recorder2.AudioRecorder);
+            _connector2.Connect(_camera2.VideoChannel, _mpeg4Recorder2.VideoRecorder);
+        }
+
+        private void Mpeg4Recorder_MultiplexFinished2(object sender, VoIPEventArgs<bool> e)
+        {
+            _connector2.Disconnect(_camera2.AudioChannel, _mpeg4Recorder2.AudioRecorder);
+            _connector2.Disconnect(_camera2.VideoChannel, _mpeg4Recorder2.VideoRecorder);
+            _mpeg4Recorder2.Dispose();
+        }
+
+
+
+        private void StopVideoCapture2()
+        {
+            if (_mpeg4Recorder2 != null)
+            {
+                _mpeg4Recorder2.Multiplex();
+                _connector2.Disconnect(_camera2.AudioChannel, _mpeg4Recorder2.AudioRecorder);
+                _connector2.Disconnect(_camera2.VideoChannel, _mpeg4Recorder2.VideoRecorder);
+            }
+        }
+
+        // luong xu ly cam 3
+        private void Record3_Click(object sender, RoutedEventArgs e)
+        {
+            var path = txtPath3.Text;
+            if (!String.IsNullOrEmpty(path))
+                StartVideoCapture3(path);
+        }
+
+        private void Stop3_Click(object sender, RoutedEventArgs e)
+        {
+            StopVideoCapture3();
+        }
+
+        private void ConnectCam3_Click(object sender, RoutedEventArgs e)
+        {
+            _camera3 = IPCameraFactory.GetCamera(IpCam3.Text, UseCam3.Text, PassCam3.Text);
+
+            _connector3.Connect(_camera3.VideoChannel, _drawingImageProvider3);
+            _camera3.Start();
+            viewVid3.Start();
+        }
+
+        private void FilePath3_Click(object sender, RoutedEventArgs e)
+        {
+            var result = _folderBrowserDialog3.ShowDialog();
+            if (result == System.Windows.Forms.DialogResult.OK)
+                txtPath3.Text = _folderBrowserDialog3.SelectedPath;
+        }
+        private void StartVideoCapture3(string path)
+        {
+            var date = DateTime.Now.Year + "y-" + DateTime.Now.Month + "m-" + DateTime.Now.Day + "d-" +
+                       DateTime.Now.Hour + "h-" + DateTime.Now.Minute + "m-" + DateTime.Now.Second + "s";
+            string currentpath;
+            if (String.IsNullOrEmpty(path))
+                currentpath = date + ".mp4";
+            else
+                currentpath = path + "\\" + date + ".mp4";
+
+            _mpeg4Recorder3 = new MPEG4Recorder(currentpath);
+            _mpeg4Recorder3.MultiplexFinished += Mpeg4Recorder_MultiplexFinished3;
+            _connector3.Connect(_camera3.AudioChannel, _mpeg4Recorder3.AudioRecorder);
+            _connector3.Connect(_camera3.VideoChannel, _mpeg4Recorder3.VideoRecorder);
+        }
+
+        private void Mpeg4Recorder_MultiplexFinished3(object sender, VoIPEventArgs<bool> e)
+        {
+            _connector3.Disconnect(_camera3.AudioChannel, _mpeg4Recorder3.AudioRecorder);
+            _connector3.Disconnect(_camera3.VideoChannel, _mpeg4Recorder3.VideoRecorder);
+            _mpeg4Recorder3.Dispose();
+        }
+
+
+
+        private void StopVideoCapture3()
+        {
+            if (_mpeg4Recorder3 != null)
+            {
+                _mpeg4Recorder3.Multiplex();
+                _connector3.Disconnect(_camera3.AudioChannel, _mpeg4Recorder3.AudioRecorder);
+                _connector3.Disconnect(_camera3.VideoChannel, _mpeg4Recorder3.VideoRecorder);
+            }
+        }
+
+        // luong xu ly cam 4
+        private void Record4_Click(object sender, RoutedEventArgs e)
+        {
+            var path = txtPath4.Text;
+            if (!String.IsNullOrEmpty(path))
+                StartVideoCapture4(path);
+        }
+
+        private void Stop4_Click(object sender, RoutedEventArgs e)
+        {
+            StopVideoCapture4();
+        }
+
+        private void ConnectCam4_Click(object sender, RoutedEventArgs e)
+        {
+            _camera4 = IPCameraFactory.GetCamera(IpCam4.Text, UseCam4.Text, PassCam4.Text);
+
+            _connector4.Connect(_camera4.VideoChannel, _drawingImageProvider4);
+            _camera4.Start();
+            viewVid4.Start();
+        }
+
+        private void FilePath4_Click(object sender, RoutedEventArgs e)
+        {
+            var result = _folderBrowserDialog4.ShowDialog();
+            if (result == System.Windows.Forms.DialogResult.OK)
+                txtPath4.Text = _folderBrowserDialog4.SelectedPath;
+        }
+
+        private void StartVideoCapture4(string path)
+        {
+            var date = DateTime.Now.Year + "y-" + DateTime.Now.Month + "m-" + DateTime.Now.Day + "d-" +
+                       DateTime.Now.Hour + "h-" + DateTime.Now.Minute + "m-" + DateTime.Now.Second + "s";
+            string currentpath;
+            if (String.IsNullOrEmpty(path))
+                currentpath = date + ".mp4";
+            else
+                currentpath = path + "\\" + date + ".mp4";
+
+            _mpeg4Recorder4 = new MPEG4Recorder(currentpath);
+            _mpeg4Recorder4.MultiplexFinished += Mpeg4Recorder_MultiplexFinished4;
+            _connector4.Connect(_camera4.AudioChannel, _mpeg4Recorder4.AudioRecorder);
+            _connector4.Connect(_camera4.VideoChannel, _mpeg4Recorder4.VideoRecorder);
+        }
+
+        private void Mpeg4Recorder_MultiplexFinished4(object sender, VoIPEventArgs<bool> e)
+        {
+            _connector4.Disconnect(_camera4.AudioChannel, _mpeg4Recorder4.AudioRecorder);
+            _connector4.Disconnect(_camera4.VideoChannel, _mpeg4Recorder4.VideoRecorder);
+            _mpeg4Recorder4.Dispose();
+        }
+
+
+
+        private void StopVideoCapture4()
+        {
+            if (_mpeg4Recorder4 != null)
+            {
+                _mpeg4Recorder4.Multiplex();
+                _connector4.Disconnect(_camera4.AudioChannel, _mpeg4Recorder4.AudioRecorder);
+                _connector4.Disconnect(_camera4.VideoChannel, _mpeg4Recorder4.VideoRecorder);
+            }
+        }
+
+        // luong xu ly cam 4
+
+
+
+    }
+}
---
+++ panzitzoom/panzitzoom/MainWindow.xaml.cs
@@ -0,0 +1,208 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Ozeki.Media;
+using Ozeki.Camera;
+using System.Globalization;
+using System.Drawing;
+namespace panzitzoom
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private IIPCamera _camera;
+        private DrawingImageProvider _drawingImageProvider =new DrawingImageProvider();
+        private MediaConnector _connector = new MediaConnector();
+
+        private List<DiscoveredDeviceInfo> _deviceList;
+        public MainWindow()
+        {
+            InitializeComponent();
+            _drawingImageProvider = new DrawingImageProvider();
+            _connector = new MediaConnector();
+            videoViewer.SetImageProvider(_drawingImageProvider);
+            IPCameraFactory.DeviceDiscovered += IPCamera_DiscoveryCompleted;
+        }
+
+        private void Connect_Click(object sender, RoutedEventArgs e)
+        {
+           // _camera = new IPCamera(txtIp.Text, txtUser.Text, txtPass.Text);
+            _camera = IPCameraFactory.GetCamera(txtIp.Text, txtUser.Text, txtPass.Text);
+
+            _connector.Connect(_camera.VideoChannel, _drawingImageProvider);
+            _camera.Start();
+            videoViewer.Start();
+            
+        }
+
+        private void MouseDownMove(object sender, MouseButtonEventArgs e)
+        {
+            var button = sender as Button;
+            if (button != null) Move(button.Content.ToString());
+        }
+
+        private void MouseUpMove(object sender, MouseButtonEventArgs e)
+        {
+            if (_camera != null)
+                _camera.CameraMovement.StopMovement();
+        }
+
+        private void Move(string direction)
+        {
+            if (_camera == null) return;
+            switch (direction)
+            {
+                case "Up Left":
+                    _camera.CameraMovement.ContinuousMove(MoveDirection.LeftUp);
+                    break;
+                case "Up":
+                    _camera.CameraMovement.ContinuousMove(MoveDirection.Up);
+                    break;
+                case "Up Right":
+                    _camera.CameraMovement.ContinuousMove(MoveDirection.RightUp);
+                    break;
+                case "Left":
+                    _camera.CameraMovement.ContinuousMove(MoveDirection.Left);
+                    break;
+                case "Right":
+                    _camera.CameraMovement.ContinuousMove(MoveDirection.Right);
+                    break;
+                case "Down Left":
+                    _camera.CameraMovement.ContinuousMove(MoveDirection.LeftDown);
+                    break;
+                case "Down":
+                    _camera.CameraMovement.ContinuousMove(MoveDirection.Down);
+                    break;
+                case "Down Right":
+                    _camera.CameraMovement.ContinuousMove(MoveDirection.RightDown);
+                    break;
+                case "In":
+                    //_camera.CameraMovement.Zoom(CameraMovement.In);
+                    _camera.CameraMovement.Zoom(MoveDirection.In);
+                    break;
+                case "Out":
+                    _camera.CameraMovement.Zoom(MoveDirection.Out);
+                    break;
+            }
+        }
+        private void button_Discover_Click(object sender, RoutedEventArgs e)
+        {
+            _deviceList = new List<DiscoveredDeviceInfo>();
+
+            InvokeGuiThread(() => comboBox_Devices.Items.Clear());
+
+            IPCameraFactory.DiscoverDevices();
+        }
+
+        private void IPCamera_DiscoveryCompleted(object sender, DiscoveryEventArgs e)
+        {
+            _deviceList.Add(e.Device);
+            InvokeGuiThread(() => comboBox_Devices.Items.Add(e.Device.Name));
+        }
+
+        private void comboBox_Devices_SelectedIndexChanged(object sender, SelectionChangedEventArgs e)
+        {
+            InvokeGuiThread(() =>
+            {
+                var selected = comboBox_Devices.SelectedIndex;
+                if (selected < 0 || selected > _deviceList.Count - 1) return;
+                textBox_Host.Text = _deviceList[selected].Host;
+                textBox_Port.Text = _deviceList[selected].Port.ToString();
+            });
+        }
+        // Setting the initial values of the scroll bars
+        private void Camera_CameraStateChanged(object sender, CameraStateEventArgs e)
+        {
+            switch (e.State)
+            {
+                case CameraState.Streaming:
+                    InvokeGUI(() =>
+                    {
+                        if (_camera.ImagingSettings.BrightnessInterval != null)
+                        {
+                            TrackBarBrightness.Minimum = (int)_camera.ImagingSettings.BrightnessInterval.Min;
+                            TrackBarBrightness.Maximum = (int)_camera.ImagingSettings.BrightnessInterval.Max;
+                            TrackBarBrightness.Value = (int)_camera.ImagingSettings.Brightness;
+                        }
+                        if (_camera.ImagingSettings.ContrastInterval != null)
+                        {
+                            TrackBarContrast.Minimum = (int)_camera.ImagingSettings.ContrastInterval.Min;
+                            TrackBarContrast.Maximum = (int)_camera.ImagingSettings.ContrastInterval.Max;
+                            TrackBarContrast.Value = (int)_camera.ImagingSettings.Contrast;
+                        }
+                        if (_camera.ImagingSettings.ColorSaturationInterval != null)
+                        {
+                            TrackBarSaturation.Minimum = (int)_camera.ImagingSettings.ColorSaturationInterval.Min;
+                            TrackBarSaturation.Maximum = (int)_camera.ImagingSettings.ColorSaturationInterval.Max;
+                            TrackBarSaturation.Value = (int)_camera.ImagingSettings.ColorSaturation;
+                        }
+
+                        BrightnessLabel.Content = _camera.ImagingSettings.Brightness.ToString(CultureInfo.InvariantCulture);
+                        ContrastLabel.Content = _camera.ImagingSettings.Contrast.ToString(CultureInfo.InvariantCulture);
+                        SaturationLabel.Content = _camera.ImagingSettings.ColorSaturation.ToString(CultureInfo.InvariantCulture);
+                    });
+
+                    break;
+            }
+        }
+
+        // Setting the camera's image
+        private void TrackBarBrightness_Scroll(object sender, System.Windows.Input.MouseButtonEventArgs e)
+        {
+            if (_camera != null)
+            {
+                _camera.ImagingSettings.SetAttributes(new CameraImaging { Brightness = Convert.ToSingle(TrackBarBrightness.Value) });
+                _camera.ImagingSettings.RefreshProperties();
+                BrightnessLabel.Content = _camera.ImagingSettings.Brightness.ToString(CultureInfo.InvariantCulture);
+            }
+        }
+
+        private void TrackBarContrast_Scroll(object sender, System.Windows.Input.MouseButtonEventArgs e)
+        {
+            if (_camera != null)
+            {
+                _camera.ImagingSettings.SetAttributes(new CameraImaging { Contrast = Convert.ToSingle(TrackBarContrast.Value) });
+                _camera.ImagingSettings.RefreshProperties();
+                ContrastLabel.Content = _camera.ImagingSettings.Contrast.ToString(CultureInfo.InvariantCulture);
+            }
+        }
+
+        private void TrackBarSaturation_Scroll(object sender, System.Windows.Input.MouseButtonEventArgs e)
+        {
+            if (_camera != null)
+            {
+                _camera.ImagingSettings.SetAttributes(new CameraImaging { ColorSaturation = Convert.ToSingle(TrackBarSaturation.Value) });
+                _camera.ImagingSettings.RefreshProperties();
+                SaturationLabel.Content = _camera.ImagingSettings.ColorSaturation.ToString(CultureInfo.InvariantCulture);
+            }
+        }
+        private void InvokeGuiThread(Action action)
+        {
+            Dispatcher.BeginInvoke(action);
+        }
+        private void InvokeGUI(Action action)
+        {
+            Dispatcher.BeginInvoke(action);
+        }
+
+        //Cameraviewwr
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            CameraViewer cam = new CameraViewer();
+                cam.Show();
+        }
+    }
+}
